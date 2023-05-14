e1940e240609a34865ffb4818b53b53008c56f1e
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2023-05-14 09:48:18 +0100

    workoing out in server

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const nodemailer = require('nodemailer');
-const Mailgen = require('mailgen');
+// const Mailgen = require('mailgen');
 // const { EMAIL, PASSWORD } = require('../.env')
 //require('dotenv').config();
 
