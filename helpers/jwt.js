8625d64fd863f493d38804fc6f6a67a1fac65fab
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2023-06-01 15:13:01 +0100

    New update

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,6 +14,7 @@ function authJwt() {
             {url: /\/public\/userUploads(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS','POST'] },
             {url: /\/api\/v1\/restaurants(.*)/ , methods: ['GET', 'OPTIONS','POST'] },
+            {url: /\/api\/v1\/riders(.*)/ , methods: ['GET', 'OPTIONS','POST'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
             {url: /\/api\/v1\/users(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
